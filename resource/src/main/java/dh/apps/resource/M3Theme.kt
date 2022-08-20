f0c28d7ca7dd879ab61a9b0766f3507afea60529
shindonghwi <ehdgnl8940@naver.com>
2022-08-20 18:26:31 +0900

    feat: roboto 폰트 추가 및 material3 텍스트 스타일 적용

--- resource/src/main/java/dh/apps/resource/M3Theme.kt
+++ resource/src/main/java/dh/apps/resource/M3Theme.kt
@@ -1,12 +1,12 @@
 package dh.apps.resource
 
-import androidx.compose.material3.lightColorScheme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -56,7 +56,7 @@ fun Android_MessageForwardTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = RobotoTypography,
         content = content
     )
 }
--- resource/src/main/java/dh/apps/resource/Type.kt
+++ resource/src/main/java/dh/apps/resource/Type.kt
@@ -1,34 +1,174 @@
+@file:OptIn(ExperimentalUnitApi::class)
+
 package dh.apps.resource
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_black),
+    Font(R.font.roboto_black_italic),
+    Font(R.font.roboto_bold),
+    Font(R.font.roboto_bold_italic),
+    Font(R.font.roboto_italic),
+    Font(R.font.roboto_light),
+    Font(R.font.roboto_light_italic),
+    Font(R.font.roboto_medium),
+    Font(R.font.roboto_medium_italic),
+    Font(R.font.roboto_regular),
+    Font(R.font.roboto_thin),
+    Font(R.font.roboto_thin_italic),
+)
+
+val RobotoTypography = Typography(
+
+    displayLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+    ),
+
+    displayMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+    ),
+
+    displaySmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+    ),
+
+    headlineSmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+    ),
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = TextUnit(0.1f, TextUnitType.Sp)
     ),
+
+    titleMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = TextUnit(0.1f, TextUnitType.Sp)
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = TextUnit(0.1f, TextUnitType.Sp)
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = TextUnit(0.5f, TextUnitType.Sp)
+    ),
+
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = TextUnit(0.5f, TextUnitType.Sp)
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = TextUnit(0.5f, TextUnitType.Sp)
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = TextUnit(0.25f, TextUnitType.Sp)
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = TextUnit(0.4f, TextUnitType.Sp)
+    ),
+
     )
-    */
-)
+
+//
+//// Set of Material typography styles to start with
+//val Typography = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = Roboto,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
+//    /* Other default text styles to override
+//    titleLarge = TextStyle(
+//        fontFamily = Roboto,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 22.sp,
+//        lineHeight = 28.sp,
+//        letterSpacing = 0.sp
+//    ),
+//    labelSmall = TextStyle(
+//        fontFamily = Roboto,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 11.sp,
+//        lineHeight = 16.sp,
+//        letterSpacing = 0.5.sp
+//    )
+//    */
+//)
