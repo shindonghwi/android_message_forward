558fc4a843de3a7aeb4b127c473931ebda6e07db
shindonghwi <ehdgnl8940@naver.com>
2022-08-30 17:14:27 +0900

    feat: home activity add

---
+++ presentation/src/main/java/dh/apps/messageforward/features/home/HomeActivity.kt
@@ -0,0 +1,44 @@
+package dh.apps.messageforward.features.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dh.apps.resource.Android_MessageForwardTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Android_MessageForwardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    Android_MessageForwardTheme {
+        Greeting2("Android")
+    }
+}
